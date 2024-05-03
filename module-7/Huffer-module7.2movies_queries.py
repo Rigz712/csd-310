0e89b4071d4e0f251f00d24227976d91b47cd634
Rigz712 <108543758+Rigz712@users.noreply.github.com>
2024-05-03 18:07:23 -0400

    Pushing 5/3/2024

--- module-7/Huffer-module7.2movies_queries.py
+++ module-7/Huffer-module7.2movies_queries.py
@@ -1,4 +1,4 @@
-# Name, Date, Assignment Name/Number: Joe Huffer, 4/21/2024, Module 6.2 'Setup'
+# Name, Date, Assignment Name/Number: Joe Huffer, 4/21/2024, Module 7.2 Movies Queries
 # Citation: https://www.tutorialspoint.com/python_data_access/python_mysql_cursor_object.htm, https://dev.mysql.com/doc/connector-python/en/connector-python-example-connecting.html, https://realpython.com/python-f-strings/, 
 
 import mysql.connector
---
+++ module-7/Untitled-1.py
@@ -0,0 +1,2 @@
+# Name, Date, Assignment Name/Number: Joe Huffer +  4/21/2024, Module 7.2 Movies Queries
+# Citation: https://www.tutorialspoint.com/python_data_access/python_mysql_cursor_object.htm, https://dev.mysql.com/doc/connector-python/en/connector-python-example-connecting.html, https://realpython.com/python-f-strings/, 
